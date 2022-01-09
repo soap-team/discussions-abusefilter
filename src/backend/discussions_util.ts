7f40802b0845c4820698a71f5e71b5f887047f42
Kris <kris.kcnotes@gmail.com>
2022-01-09 19:05:36 +1100

    Copy over DiscussionsUtil class

---
+++ src/backend/discussions_util.ts
@@ -0,0 +1,71 @@
+import type { ApiInterface } from 'fandom-api';
+import type { ContentNode } from 'fandom-api/dist/controllers/discussion_types/json_model';
+
+export class DiscussionUtil {
+  api: ApiInterface;
+
+  constructor(api: ApiInterface) {
+    this.api = api;
+  }
+
+  convertLinkToAbsolute(link: string, wiki: string): string {
+    if (link.startsWith('/')) {
+      link = `https://${wiki}${link}`;
+    }
+    return link;
+  }
+
+  // Recursively retrieves a list of links from any node with content.
+  //  Links only appear in text and opengraph nodes
+  getLinks(doc: ContentNode, wiki: string): string[] {
+    let links: string[] = [];
+
+    if (doc && doc.content && doc.content.length > 0) {
+      doc.content.forEach(node => {
+        switch (node.type) {
+          case 'openGraph':
+            links.push(node.attrs.url);
+            break;
+          case 'text':
+            if (node.marks) {
+              node.marks.forEach(mark => {
+                if (mark.type !== 'link') {
+                  return;
+                }
+                if (mark.attrs && mark.attrs.href) {
+                  links.push(mark.attrs.href);
+                }
+              });
+            }
+            break;
+          case 'paragraph':
+          case 'bulletList':
+          case 'orderedList':
+          case 'listItem':
+          case 'code_block':
+            if (node.content) {
+              links = links.concat(this.getLinks(node, wiki));
+            }
+        }
+      });
+    }
+    return links.map(link => this.convertLinkToAbsolute(link, wiki));
+  }
+
+  getTextContent(node: ContentNode): string {
+    const text = node.content?.map((subNode) => {
+      switch (subNode.type) {
+        case 'paragraph':
+        case 'bulletList':
+        case 'orderedList':
+        case 'listItem':
+        case 'code_block':
+          return `${this.getTextContent(subNode)}`;
+        case 'text':
+          return `${subNode.text}`;
+      }
+    });
+    return text?.join('\n') || '';
+  }
+}
+
