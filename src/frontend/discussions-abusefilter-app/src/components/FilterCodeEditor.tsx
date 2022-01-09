5116d4b52c4c2d90c38009b2abec3ba3a093a1da
Kris <kris.kcnotes@gmail.com>
2022-01-09 21:08:24 +1100

    Change to uncontrolled

--- src/frontend/discussions-abusefilter-app/src/components/FilterCodeEditor.tsx
+++ src/frontend/discussions-abusefilter-app/src/components/FilterCodeEditor.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {
   Paper,
 } from '@mui/material';
-import { Controlled as CodeMirror } from 'react-codemirror2';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/addon/selection/active-line';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/zenburn.css';
@@ -12,12 +12,6 @@ import FormContext from '../contexts/FormContext';
 export default function FilterCodeEditor() {
   const [focus, setFocus] = React.useState(false);
   const { filter, modifyFilter } = React.useContext(FormContext);
-  const [code, setCode] = React.useState(filter);
-
-  const handleFilterChange = (editor: unknown, event: unknown, value: string) => {
-    setCode(value);
-    console.log(value);
-  };
 
   const handleFocusChange = () => {
     setFocus(true);
@@ -43,7 +37,7 @@ export default function FilterCodeEditor() {
       ]}>
         <CodeMirror
         // className={theme === 'light' ? undefined : "codemirror-dark"}
-          value={code}
+          value={filter}
           options={{
             mode: 'xml',
             lineNumbers: true,
@@ -51,7 +45,6 @@ export default function FilterCodeEditor() {
             styleActiveLine: true,
             screenReaderLabel: 'filter',
           }}
-          onBeforeChange={handleFilterChange}
           onFocus={handleFocusChange}
           onBlur={handleBlurChange}
         />
