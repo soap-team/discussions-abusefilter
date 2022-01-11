baa703716bb1a4c56c01e7e4dbc77dc22d271233
Kris <kris.kcnotes@gmail.com>
2022-01-11 23:02:38 +1100

    Add shared types

--- src/shared/actions.ts
+++ src/shared/actions.ts
@@ -1,21 +1,28 @@
+// All actions a filter can take
 export type RuleAction =
   | LogAction
   | DeleteAction
   | LockAction
   | ReplyAction;
 
+// Logs to Discord
 type LogAction = {
   action: 'log',
   webhook: string,
   content: string,
 };
+
+// Deletes a thread or reply
 type DeleteAction = {
   action: 'delete',
 };
+
+// Locks a thread
 type LockAction = {
   action: 'lock'
 };
 
+// Replies to a thread
 type ReplyAction = {
   action: 'reply',
   message: string,
